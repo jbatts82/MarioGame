9a8be83978743fd9617138524d9ee51757c741a4
jbatts82 <jbatts82@hotmail.com>
2022-11-20 09:31:45 -0500

    adding level 1 class attributes

--- data/state/level1.py
+++ data/state/level1.py
@@ -8,4 +8,21 @@ import pygame as pg
 from .. import constants as c
 from .. import tools
 
-class Level(tools._State)
+
+class Level(tools._State):
+    def __init__(self):
+        tools._State.__init__(self)
+
+    def startup(self, current_time, persist):
+        # calls when the state object is created
+        self.game_info = persist
+        self.persist = self.game_info
+
+        self.game_info[c.CURRENT_TIME] = current_time
+        self.game_info[c.LEVEL_STATE] = c.NOT_FROZEN
+        self.game_info[c.MARIO_DEAD] = False
+
+        self.state = c.NOT_FROZEN
+        self.death_timer = 0
+        self.flag_score = None
+
