ed2abda9f1ae8b4da56bb237ee81b50bcffabc0c
jbatts82 <jbatts82@hotmail.com>
2022-11-20 09:31:18 -0500

    adding collide detection

--- data/component/collider.py
+++ data/component/collider.py
@@ -7,15 +7,15 @@
 import pygame as pg
 from .. import constants as c
 
+
 class Collider(pg.sprite.Sprite):
-    def __init__(self,x,y,width,height,name='collider'):
+    def __init__(self, x, y, width, height, name='collider'):
         pg.sprite.Sprite.__init__(self)
-        self.image = pg.Surface((width,height)).convert()
-        #fill image with red color
+        self.image = pg.Surface((width, height)).convert()
+        # fill image with red color
 
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
 
         self.state = None
-        
